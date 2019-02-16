9c648da12c88c0c99a4781457ea2923e0cea76de
dutraneto <dudutraneto@gmail.com>
2019-02-16 14:58:42 -0500

    initial commit create header

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import './Header.css'
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+    render() {
+        return (
+            <header>
+                Hello Resume
+            </header>
+        )
+    }
+}
