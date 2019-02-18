57b28ea34c8f0bad8d67da389d0360347474a0d0
dutraneto <dudutraneto@gmail.com>
2019-02-18 00:50:49 -0500

    remove import app file

--- src/main/App.jsx
+++ src/main/App.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 import {BrowserRouter} from 'react-router-dom'
 import Header from '../components/Header'
 import Main from '../components/Main'
-import Experience from '../components/Experience'
 import Routes from './Routes'
 
 export default props =>
